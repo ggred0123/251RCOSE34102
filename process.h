a5dd7c323d56409b7fa1b3691e03c22e740ae1ff
YoungminKim <ggred0123@gmail.com>
2025-05-04 13:44:38 +0900

    first commit

---
+++ process.h
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#ifndef PROCESS_H
+#define PROCESS_H
+typedef enum {NEW, READY, RUNNING, WAITING, TERMINATED} Status;
+
+typedef struct Process {
+    int pid;//processId
+    int cpu_burst_time; //cpu burst time
+    int remaining_cpu_burst_time; // 남아있는 cpu burst
+    int io_burst_time;
+    int remaining_io_burst_time;
+    Status status;
+    int waiting_time;
+    int turnaround_time;
+    int
+
+
+
+
+
+}
+
+
+
+
+
+
+
+#endif //PROCESS_H
